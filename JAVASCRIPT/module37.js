8a509527a01b1b3dc2c587e89dd56c68ce51f466
Payalsahoo4815 <payalsahoo931@gmail.com>
2025-02-20 08:13:50 +0530

    commit

---
+++ JAVASCRIPT/module37.js
@@ -0,0 +1,16 @@
+console.log(this)
+
+// Primitive Datatypes
+// -numbers : 1,3,4.5,657
+// -String : 'payal' ,"jyoti"
+// -boolean : true,false
+// -null
+// -undefined
+
+// Non Primitive Datatypes
+// -Array
+let name = ['abc','cvb','rfcgf',76,7898,true]
+
+// -object : {}
+// for variable let use hoti hai
+// for constant const use hoti hai
---
+++ JAVASCRIPT/module37_2.js
@@ -0,0 +1,72 @@
+// Conditions and loops
+
+// let age = 14;
+// if(age >= 18){
+//     console.log("Your are Allowed");  
+// }
+// else{
+//     console.log("You are not allowed")
+// }
+
+// let signal = "red";
+// if (signal == "red"){
+//     console.log("Red => Stop");
+// }
+// else if(signal == "Yellow"){
+//     console.log("Yellow => Go Slow")
+// }
+// else if (signal == "Green"){
+//     console.log("Green => Go Fast")
+// }
+// else{
+//     console.log("Invalid")
+// }
+
+
+
+// Switch case
+
+// let user = "mother"
+// switch(user)
+// {
+//     case "Admin" : 
+//         console.log("He is Admin");
+//         break
+//     case "Student" :
+//         console.log("He is a student")
+//         break
+//     case "Mentor" :
+//         console.log("He is a Mentor")
+//         break
+//     default :
+//         console.log("Enter a valid user")     
+// }
+
+
+// Loops
+// While,do while,for
+
+// for(let i=0;i<=5;i++){
+//     console.log(i);
+// }
+
+// do while
+// let i = 0
+// do{
+//     console.log("Hello World")
+//     i++
+// }while(i<5)
+
+
+// While
+// let i = 0
+// while(i<5){
+//     console.log("Hello");
+//     i++
+// }
+
+
+// Ternary Operator or Ternary condition
+
+let islogin = false
+islogin ? console.log("Home Page") : console.log("Login page")

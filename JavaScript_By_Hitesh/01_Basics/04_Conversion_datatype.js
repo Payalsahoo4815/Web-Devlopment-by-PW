c72184d2b765018eb38adf29cf9521b4f07fac85
Payalsahoo4815 <payalsahoo931@gmail.com>
2025-04-29 07:41:00 +0530

    Basic1

---
+++ JavaScript_By_Hitesh/01_Basics/04_Conversion_datatype.js
@@ -0,0 +1,15 @@
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(+true);
+
+
+
---
+++ JavaScript_By_Hitesh/01_Basics/05_DataTypes_summary.js
@@ -0,0 +1,20 @@
+// Primitive Datatypes
+/**
+ * 7 Primitive Datatypes
+ * 
+ * 1.String
+ * 2.Number
+ * 3.Boolean
+ * 4.Null
+ * 5.Undefined
+ * 6.Symbol
+ * 7.BigInt
+ */
+
+
+
+
+// Non-Primitive or Reference Type
+/**
+ * Array,Objects,Functions
+ */
